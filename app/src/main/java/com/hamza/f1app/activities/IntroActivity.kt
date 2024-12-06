1ac6f72890e13c4cc9c66dd4d769c7ac68598540
Mohammed-Amine BAHRAOUI <mhandanin@gmail.com>
2024-12-07 00:54:02 +0100

    - add activity_intro.xml - finish activity_main.xml - change app logo - declare new background color: backgroundGeneral - add intro video: mclaren_intro.mp4 - declare IntroActivity - declare new styles: mainTitles and mainCardStyle

---
+++ app/src/main/java/com/hamza/f1app/activities/IntroActivity.kt
@@ -0,0 +1,50 @@
+
+package com.hamza.f1app.activities
+
+import android.animation.ObjectAnimator
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.VideoView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.hamza.f1app.R
+
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_intro)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.intro)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val introVideo = findViewById<VideoView>(R.id.introVideo)
+
+
+
+
+        val introVideoUri = Uri.parse("android.resource://${packageName}/raw/mclaren_intro")
+        introVideo.setVideoURI(introVideoUri)
+
+        introVideo.start()
+
+
+
+        introVideo.setOnCompletionListener {
+           val intent=Intent(this,SplachActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+
+}
--- app/src/main/java/com/hamza/f1app/activities/MainActivity.kt
+++ app/src/main/java/com/hamza/f1app/activities/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.hamza.f1app.activities
 
-import android.annotation.SuppressLint
+import android.animation.ObjectAnimator
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.VideoView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.tabs.TabLayout
 import com.hamza.f1app.R
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,5 +26,49 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+//        val introVideo = findViewById<VideoView>(R.id.introVideo)
+        val main = findViewById<LinearLayout>(R.id.main)
+        val racingCard = findViewById<CardView>(R.id.racingCard)
+        val driversCard = findViewById<CardView>(R.id.driversCard)
+        val constructorsCard = findViewById<CardView>(R.id.constructorsCard)
+        val standingsCard = findViewById<CardView>(R.id.standingsCard)
+
+        hide(racingCard)
+        hide(driversCard)
+        hide(constructorsCard)
+        hide(standingsCard)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.statusBarColor = ContextCompat.getColor(this, R.color.f1red)
+        }
+
+
+//        val introVideoUri = Uri.parse("android.resource://${packageName}/raw/mclaren_intro")
+//        introVideo.setVideoURI(introVideoUri)
+//
+//        introVideo.start()
+
+
+
+//        introVideo.setOnCompletionListener {
+//            introVideo.visibility = VideoView.GONE
+//            main.visibility = LinearLayout.VISIBLE
+            translater(racingCard, 1000)
+            translater(driversCard, 1100)
+            translater(constructorsCard, 1200)
+            translater(standingsCard, 1300)
+
+//        }
+    }
+
+    private fun translater(card: CardView, duration: Long) {
+        val animator = ObjectAnimator.ofFloat(card, View.TRANSLATION_X, 0f)
+        animator.duration = duration
+        animator.start()
+    }
+
+    private fun hide(card: CardView) {
+        val animator = ObjectAnimator.ofFloat(card, View.TRANSLATION_X, 1500f)
+        animator.repeatCount = 1
+        animator.start()
     }
 }
